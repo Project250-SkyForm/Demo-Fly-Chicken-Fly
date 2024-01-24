b6691b985d0978bcba53263e62b09091015103ce
jimmy liu <zhijianliu72@gmail.com>
2024-01-24 03:08:33 -0700

    Your message in quotes

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,16 +5,24 @@ using UnityEngine.SceneManagement;
 
 public class Player : AnimatedEntity
 {   
-    //public float Speed=5;
-    private float Gravity = -9.81f;
+    private const float Gravity = -9.81f;
+    private const float camera_const = 5;
+    private const float standing_const = 1.0f;
     private float Speed = 0;
-    private bool falling = true;
+    private bool falling = false;
+    private bool able_move_left = true;
+    private bool able_move_right = true;
     private float horizontal_speed=10.0f; //horizontal_speed_left = 10.0f, horizontal_speed_right = 10.0f;
     private AudioSource audioSource;
     private Platform current_platform;
     private SpriteRenderer spriteRenderer;
+    private int lump = 3;
+
 
     public List<Sprite> interruptAnimationCycle;
+    public BackgroundScroll background;
+    public BackgroundScroll leftRect;
+    public BackgroundScroll rightRect;
 
     void Start()
     {
@@ -29,14 +37,19 @@ public class Player : AnimatedEntity
 
         // Going up
         if (Input.GetKey(KeyCode.W) && falling==false){
-            Speed = 8;
+            if (current_platform != null){
+                    //current_platform.refresh();
+            }
+            leftRect.Continue();
+            rightRect.Continue();
+            Speed = 10;
             falling = true;
             current_platform = null;     // to avoid null bug
         }
 
         // Going left
-        if(Input.GetKey(KeyCode.A)){
-            // horizontal_speed_right = 10.0f;
+        if(Input.GetKey(KeyCode.A) && able_move_left){
+            able_move_right = true;     // if we move left, which means we have space to move right now
             transform.position+= Vector3.left*Time.deltaTime*horizontal_speed;
             if (current_platform!=null){
                 if (transform.position.x < current_platform.getLeft()){ //detect whether player go beyond the left edge of the platform
@@ -46,8 +59,8 @@ public class Player : AnimatedEntity
         }
 
         // Going down
-        if(Input.GetKey(KeyCode.D)){
-            // horizontal_speed_left = 10.0f;
+        if(Input.GetKey(KeyCode.D) && able_move_right){
+            able_move_left = true;
             transform.position+= Vector3.right*Time.deltaTime*horizontal_speed;
             if (current_platform!=null){
                 if (transform.position.x > current_platform.getRight()){ //detect whether player go beyond the right edge of the platform
@@ -57,9 +70,21 @@ public class Player : AnimatedEntity
         }
 
 
-        if (falling){
+        if (falling){   // if the player is not standing on the playform, it should fall down
             Speed += Gravity*Time.deltaTime;
             transform.position+=Vector3.up*Time.deltaTime*Speed;
+        }
+
+        Vector3 cameraPosition = Camera.main.transform.position;    //determine whether the camera is too high to see the player
+        if (cameraPosition.y > transform.position.y + camera_const){    //stop the motions of both the background and camera and left & right rect
+            Interrupt (interruptAnimationCycle);
+            lump -= 1;
+            Vector3 newPos = Camera.main.transform.position;
+            newPos.y = transform.position.y + camera_const;
+            Camera.main.transform.position = newPos;
+            background.Stop();
+            leftRect.Stop();
+            rightRect.Stop();
         }
         
     }
@@ -68,29 +93,35 @@ public class Player : AnimatedEntity
         Platform platform = other.gameObject.GetComponent<Platform>();
 
         if(platform!=null){
-            if (audioSource != null)
-            {
-                audioSource.Play();
+            if (platform.getType()=="left_rect"){
+                able_move_left = false;
+                Debug.Log("Collide left rect");
             }
-            Interrupt (interruptAnimationCycle);
-            //  //we don't want the player move to left when encountering a platform on left 
-            //         if (CalculateEdges("left") >= platform.getRight()){
-            //             horizontal_speed_left = 0;
-            //         }
-                
-            //   //we don't want the player move to right when encountering a platform on left
-            //         if (CalculateEdges("right")>= platform.getLeft()){
-            //             horizontal_speed_right = 0;
-            //         }
-            
-            if (transform.position.y >= platform.getTop()){
-                Speed = 0;
-                falling = false;
-                current_platform = platform;    //record the information of the collider maybe
+            else if (platform.getType()=="right_rect"){
+                able_move_right = false;
+                Debug.Log("Collide right rect");
             }
             else{
-                Speed = 0;
+                if (audioSource != null){
+                    audioSource.Play();
+                }
+                if (falling && transform.position.y>=platform.transform.position.y){    // depature on the platform
+                    Speed = 0;
+                    falling = false;
+                    current_platform = platform;    //record the information of the collider maybe
+                    Debug.Log("Depature on playform");
+                }
+                else if (Speed>=0){         //going up and hit the platform above
+                    Speed = 0;   
+                    current_platform = platform;
+                    falling = true;
+                    Interrupt (interruptAnimationCycle);
+                    lump -= 1;
+                    Debug.Log("hit the platform");
+                }
+
             }
+           
         }
     }
 
@@ -103,10 +134,10 @@ public class Player : AnimatedEntity
         if (spriteRenderer != null)
         {
             // Get the left and right edges of the platform
-            float leftEdge = transform.position.x - spriteRenderer.bounds.size.x / 2f;
-            float rightEdge = transform.position.x + spriteRenderer.bounds.size.x / 2f;
-            float topEdge = transform.position.y + spriteRenderer.bounds.size.y / 2f;
-            float downEdge = transform.position.y - spriteRenderer.bounds.size.y / 2f;
+            float leftEdge =  - spriteRenderer.bounds.size.x / 2f;
+            float rightEdge =  spriteRenderer.bounds.size.x / 2f;
+            float topEdge =  spriteRenderer.bounds.size.y / 2f;
+            float downEdge = - spriteRenderer.bounds.size.y / 2f;
             if (direction=="left"){
                 return leftEdge;
             }
@@ -117,7 +148,7 @@ public class Player : AnimatedEntity
                 return topEdge;
             }
             else if (direction == "down"){
-                Debug.Log("Down"+ downEdge);
+                //Debug.Log("Down"+ downEdge);
                 return downEdge;
             }
             else{   // should not happen, just in case debug warning
